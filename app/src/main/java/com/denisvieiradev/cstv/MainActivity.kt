283c5891ab0bf75fccc1744f45bb8e842a89c93d
Denis Vieira <denisvieira05@gmail.com>
2026-03-10 21:30:40 -0300

    feat: migrate theme to design-system module and update primary color palette
    - Delete default theme files (`Color.kt`, `Theme.kt`, `Type.kt`) from the `:app` module.
    - Update `MainActivity` to use `CstvTheme` from the `:design-system` module.
    - Refactor the design system's primary color palette, changing the base color from green to red for both light and dark themes.

--- app/src/main/java/com/denisvieiradev/cstv/MainActivity.kt
+++ app/src/main/java/com/denisvieiradev/cstv/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.denisvieiradev.cstv.ui.theme.CstvTheme
+import com.denisvieiradev.design_system.ui.theme.CstvTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
